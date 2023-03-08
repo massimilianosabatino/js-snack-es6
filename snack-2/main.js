534bd7800974b050626140b1153441173187d906
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-03-08 18:38:41 +0100

    add lista con tutte le chiavi su richiesta 1

--- snack-2/main.js
+++ snack-2/main.js
@@ -40,12 +40,22 @@ const students = [
 
 //Lista con nome studente in maiuscolo
 const studentName = students.map((student) => {
-    return {name: student.name.toLocaleUpperCase()};
+    return {name: student.name.toUpperCase()};
 })
+/*
+//Lista con tutte le chiavi incluse
+const studentName = students.map((student) => {
+    return {
+        ...student,
+        name: student.name.toUpperCase()
+    };
+})
+*/
 
 console.log('Lista richiesta 1');
 studentName.forEach((element) => console.log(element.name));
 
+console.log('----');
 
 //Lista richiesta con voto superiore a 70
 const studentSuper70 = students.filter(student => student.grades > 70);
@@ -53,6 +63,7 @@ const studentSuper70 = students.filter(student => student.grades > 70);
 console.log('Lista richiesta 2');
 studentSuper70.forEach(student => console.log(student.name + ' - ' + 'voto: ' + student.grades));
 
+console.log('----');
 
 //Lista richiesta con voto superiore a 70 e id superiore di 120
 const studentSuperTop = students.filter(student => student.grades > 70 && student.id > 120);
