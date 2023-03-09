22b830db8696b9c7630d4665c704b94ca02f5ffa
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-03-09 16:04:09 +0100

    add bikes object

---
+++ snack-3/main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const bikes = [
+    {
+        name: 'Atala',
+        weight: 16
+    },
+    {
+        name: 'Basso',
+        weight: 12
+    },
+    {
+        name: 'Bianchi',
+        weight: 13
+    },
+    {
+        name: 'Bottecchia',
+        weight: 10
+    },
+    {
+        name: 'Carrera',
+        weight: 8
+    },
+    {
+        name: 'Lombardo',
+        weight: 9
+    },
+    {
+        name: 'Coppi',
+        weight: 15
+    },
+    {
+        name: 'Cipollini',
+        weight: 11
+    },
+    {
+        name: 'Bartolomei',
+        weight: 15
+    }
+];
+
