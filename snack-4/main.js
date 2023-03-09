a5a323baa2afd1f7bb95f99ff791158abe08ddcb
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-03-09 17:50:45 +0100

    add teams object

---
+++ snack-4/main.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const teams = [
+    {
+        name: 'Juventus',
+        pointsMade: 0,
+        foulsSuffered: 0
+    },
+    {
+        name: 'Palermo',
+        pointsMade: 0,
+        foulsSuffered: 0
+    },
+    {
+        name: 'Milan',
+        pointsMade: 0,
+        foulsSuffered: 0
+    },
+    {
+        name: 'Inter',
+        pointsMade: 0,
+        foulsSuffered: 0
+    },
+    {
+        name: 'Bologna',
+        pointsMade: 0,
+        foulsSuffered: 0
+    },
+    {
+        name: 'Atalanta',
+        pointsMade: 0,
+        foulsSuffered: 0
+    },
+    {
+        name: 'Genoa',
+        pointsMade: 0,
+        foulsSuffered: 0
+    },
+    {
+        name: 'Sevilla',
+        pointsMade: 0,
+        foulsSuffered: 0
+    },
+    {
+        name: 'Barcellona',
+        pointsMade: 0,
+        foulsSuffered: 0
+    },
+    {
+        name: 'Città di Castellana',
+        pointsMade: 0,
+        foulsSuffered: 0
+    }
+]
